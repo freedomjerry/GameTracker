32b1783a586519f33c6f9f440ab77ebfde51e0fd
hotjerry <254406949@qq.com>
2022-08-04 09:56:43 +0800

    new directory

--- cmd/webserver/main.go
+++ cmd/webserver/main.go
@@ -4,8 +4,10 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"github.com/GameTracker"
 )
 const dbFileName = "game.db.json"
+
 func main()  {
 
 	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
@@ -14,12 +16,12 @@ func main()  {
 		log.Fatalf("problem opening %s, %v", dbFileName, err)
 	}
 
-	store, err := NewFileSystemPlayerStore(db)
+	store, err := poker.NewFileSystemPlayerStore(db)
 	if err != nil {
 		log.Fatalf("problem creating file system player store, %v ", err)
 	}
 
-	server := NewPlayerServer(store)
+	server := poker.NewPlayerServer(store)
 	if err := http.ListenAndServe(":5000", server); err != nil {
 		log.Fatalf(" could not listen on port 5000 %v ", err)
 	}
